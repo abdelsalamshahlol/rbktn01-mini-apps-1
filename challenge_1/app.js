cea6e0ce53f62882c37d6dc779b2a85eca576c92
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-27 12:31:38 +0100

    basic board setup on app file

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,18 +1,25 @@
 let fields = document.getElementsByClassName('field');
 let move = 'X';
-let board = {
-  0: new Array(3),
-  1: new Array(3),
-  2: new Array(3)
-};
+let board = [
+  new Array(3),
+  new Array(3),
+  new Array(3)
+];
+
+let playMove = (e) => {
+  let position = e.target.dataset;
+  if (!validateMove(position)) {
+    move = move === 'X' ? 'O' : 'X';
+    board[position.x][position.y] = move
+    e.target.children[0].innerHTML = move;
+  }
+  console.log(board, position)
+}
+
+let validateMove = (position) => {
+  return board[position.x][position.y];
+}
 
 for (let field of fields) {
-  field.onclick = (e) => {
-    field.children[0].innerHTML = move;
-    move = move === 'X' ? 'O' : 'X';
-    let position = field.dataset;
-    console.log(board, position)
-  }
+  field.onclick = playMove;
 }
-console.log(fields)
-
