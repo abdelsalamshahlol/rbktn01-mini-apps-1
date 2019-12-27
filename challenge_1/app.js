792bf2d91af36b8c30ac5414d7aa6aeca5079291
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-27 11:47:26 +0100

    added nav with button to HTML and fixed style

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,10 @@
 let fields = document.getElementsByClassName('field');
+let move = 'X';
+
 for (let field of fields) {
   field.onclick = (e) => {
-    field.children[0].innerHTML = 'X';
+    field.children[0].innerHTML = move;
+    move = move === 'X' ? 'O' : 'X';
   }
 }
 console.log(fields)
