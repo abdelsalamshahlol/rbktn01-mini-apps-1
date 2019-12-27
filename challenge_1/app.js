b48b247ba2e0fba9e6f7552a7abbcffc2339a80e
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-27 15:16:13 +0100

    basic horizontal check concept

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,9 +1,9 @@
 let fields = document.getElementsByClassName('field');
 let move = 'X';
 let board = [
-  new Array(3),
-  new Array(3),
-  new Array(3)
+  Array.from({ length: 3 }, () => 0),
+  Array.from({ length: 3 }, () => 0),
+  Array.from({ length: 3 }, () => 0)
 ];
 
 let playMove = (e) => {
@@ -15,14 +15,31 @@ let playMove = (e) => {
   let position = e.target.dataset;
   if (!validateMove(position)) {
     move = move === 'X' ? 'O' : 'X';
-    board[position.x][position.y] = move
+    board[position.x][position.y] = move === 'X' ? 1 : 2
     e.target.children[0].innerHTML = move;
+    // Check if game is
+    checkBoardHorizontal()
   }
-  console.log(board, position)
+  // console.log(board, position)
 }
 
 let validateMove = (position) => {
   return board[position.x][position.y];
+}
+
+let checkBoardHorizontal = () => {
+  console.log(board)
+  for (let row of board) {
+    console.log(row.reduce((acc, nemo) => acc + nemo, ''));
+    if (row.reduce((acc, memo) => acc + memo) === 6) {
+      console.log('Game over O wins')
+      // return true;
+    }
+    if (row.reduce((acc, memo) => acc + memo) === 3) {
+      console.log('Game over X wins')
+      // return true;
+    }
+  }
 }
 
 for (let field of fields) {
