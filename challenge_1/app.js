a7c897186cbefd8736fc50c50666c0004cf21118
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-27 11:33:24 +0100

    updated html and style for fields

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,7 @@
 let fields = document.getElementsByClassName('field');
 for (let field of fields) {
   field.onclick = (e) => {
-    console.log(e)
+    field.children[0].innerHTML = 'X';
   }
 }
 console.log(fields)
