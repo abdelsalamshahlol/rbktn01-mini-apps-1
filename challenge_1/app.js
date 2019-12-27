09ed150deb3034bf1fa5b56be0b2b2f4dbdcc51f
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-27 12:08:27 +0100

    click position sorted

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,10 +1,17 @@
 let fields = document.getElementsByClassName('field');
 let move = 'X';
+let board = {
+  0: new Array(3),
+  1: new Array(3),
+  2: new Array(3)
+};
 
 for (let field of fields) {
   field.onclick = (e) => {
     field.children[0].innerHTML = move;
     move = move === 'X' ? 'O' : 'X';
+    let position = field.dataset;
+    console.log(board, position)
   }
 }
 console.log(fields)
