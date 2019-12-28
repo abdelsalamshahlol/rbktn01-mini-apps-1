229ee2ceb29408f1509ea6b854a785aab31d2615
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-28 10:32:54 +0100

    basic server setup

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send('home page');
+});
+
+app.post('/parse', (req, res) => {
+
+});
+
+app.listen(port, () => console.log(`App running on port ${port}.`));
