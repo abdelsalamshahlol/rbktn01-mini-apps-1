549f174c5e5bb7b7f26d28991995e1d58b55022b
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-28 10:57:17 +0100

    form data captured

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+
+const form = new multer();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('home page');
 });
 
-app.post('/parse', (req, res) => {
-
+app.post('/parse', form.none(), (req, res) => {
+  let content = req.body.content;
+  console.log(req.body)
+  res.json(content);
 });
 
 app.listen(port, () => console.log(`App running on port ${port}.`));
