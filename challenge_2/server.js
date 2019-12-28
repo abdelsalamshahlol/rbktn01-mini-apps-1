27d168abb154a6d3531abc7b2695546e58469ec4
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-28 15:32:42 +0100

    JSON to CSV done

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -15,16 +15,58 @@ app.get('/', (req, res) => {
   // res.send('home page');
 });
 
-app.post('/parse', form.none(), (req, res) => {
+var columnsCSV = function (obj) {
+  let columns = '';
+  let tmp = Object.keys(obj);
+
+  for (let i = 0; i < tmp.length; i++) {
+    if (tmp[i] !== 'children') {
+      columns += tmp[i];
+      if (i < tmp.length - 2) {
+        columns += ',';
+      } else {
+        columns += '\n';
+      }
+    }
+  }
+  return columns;
+}
+
+var flatten = function (obj) {
+  let res = arguments[1] || '';
+  let data = Object.values(obj);
+  for (let i = 0; i < data.length; i++) {
+    if (typeof data[i] === 'object') {
+      res = flatten(data[i], res);
+    } else {
+      res += data[i];
+      if (i !== data.length - 2) {
+        res += ',';
+      } else {
+        res += '\n';
+      }
+    }
+  }
+  return res;
+}
+
+app.post('/parse', form.single('content'), (req, res) => {
   let content = req.body.content;
   if (content) {
     // convert the object to CSV string
-    let x = JSON.parse(content);
-    res.json(x);
+    let result = '';
+    try {
+      // Validate input to check if its JSON
+      let object = JSON.parse(content);
+      result = columnsCSV(object) + flatten(object);
+    } catch (e) {
+      res.status(422).send(e);
+      return;
+    }
+    res.send(result);
   } else {
     res.sendStatus(422);
   }
-
 });
 
 app.listen(port, () => console.log(`App running on port ${port}.`));
