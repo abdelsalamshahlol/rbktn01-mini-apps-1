d10ae37e3269b357568800567d0b45ffa6af8890
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-30 10:05:20 +0100

    basic express server done

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+// express app
+const express = require('express');
+const app = new express();
+const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
 
+app.listen(port, () => {
+  console.log('server running on port ' + port);
+});
+
+app.get('/', (req, res) => {
+  // res.end('end')
+});
