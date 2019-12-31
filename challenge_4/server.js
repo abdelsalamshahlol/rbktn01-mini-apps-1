9173097176dbb5917c17a24a9a40970247b60b0a
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-31 10:56:43 +0100

    basic express app working

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
