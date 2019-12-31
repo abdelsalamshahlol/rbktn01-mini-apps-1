8e5914158d6204458bab6a50b39811d0a1f62ec3
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-31 11:35:45 +0100

    webpack config is done

--- challenge_4/webpack.config.js
+++ challenge_4/webpack.config.js
@@ -1,4 +1,8 @@
+const path = require('path');
+
 module.exports = {
+  mode: "development",
+  entry: "./client/app.jsx",
   module: {
     rules: [{
       test: /\.(js|jsx)$/,
@@ -6,6 +10,16 @@ module.exports = {
       use: {
         loader: 'babel-loader'
       }
-    }]
-  }
+    }],
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, 'public/resources')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
 }
